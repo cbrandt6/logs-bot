018bbdb290c9ae40b27a2ebf2fa7b322f1136e24
Collin <cbrandt6@ycp.edu>
2024-01-10 20:52:26 -0500

    Working on getting scoreboard messages, using message id

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -31,8 +31,9 @@ func main() {
 		fmt.Println("Error connecting", err)
 		return
 	}
-	bot.ReadMessagesAllFromChannel()
+	bot.GetAllMessagesFromChannel()
 
+	bot.Open()
 	fmt.Println("Bot is now running. Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, syscall.SIGTERM)
--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -2,35 +2,46 @@ package bot
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 type DiscordBot struct {
-	token            string
-	currentChannelId string
-	currentSession   *discordgo.Session
+	token     string
+	channelId string
+	session   *discordgo.Session
 }
 
 func NewBot(token string, channelId string) *DiscordBot {
 	bot := new(DiscordBot)
 	bot.token = token
-	bot.currentChannelId = channelId
+	bot.channelId = channelId
 
 	return bot
 }
 
 func (b *DiscordBot) Connect() error {
 	var err error
-	b.currentSession, err = discordgo.New("Bot " + b.token)
-	//b.currentSession.AddHandler(b.messageCreate)
+	b.session, err = discordgo.New("Bot " + b.token)
+	b.session.AddHandler(b.messageCreate)
 	return err
 }
 
-func (b DiscordBot) messageCreate(m *discordgo.MessageCreate) {
+func (b *DiscordBot) Open() {
+	b.session.Open()
+	fmt.Println("Bot session opened")
+}
+
+func (b *DiscordBot) Close() {
+	b.session.Close()
+	fmt.Println("Bot session closed")
+}
+
+func (b *DiscordBot) messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	fmt.Println("Message received: ", m.Content)
-	if m.Author.ID == b.currentSession.State.User.ID {
+	if m.Author.ID == b.session.State.User.ID {
 		fmt.Println("Ignoring bot message")
 		return
 	}
@@ -44,38 +55,35 @@ func (b DiscordBot) messageCreate(m *discordgo.MessageCreate) {
 	}
 }
 
-func (b DiscordBot) handleCommand(m *discordgo.MessageCreate) {
+func (b *DiscordBot) handleCommand(m *discordgo.MessageCreate) {
 	switch m.Content {
 	case "!ping":
 		b.SendMessage("pong")
 	}
 }
 
-func (b DiscordBot) SendMessage(message string) {
-	_, err := b.currentSession.ChannelMessageSend(b.currentChannelId, message)
+func (b *DiscordBot) SendMessage(message string) {
+	_, err := b.session.ChannelMessageSend(b.channelId, message)
 	if err != nil {
 		fmt.Printf("Error sending message: %v\n", err)
 	}
 }
 
-func (b DiscordBot) ReadMessagesAllFromChannel() {
-	messages, err := b.currentSession.ChannelMessages(b.currentChannelId, 100, "", "", "")
+func (b *DiscordBot) GetAllMessagesFromChannel(limit int, beforeId string, afterId string, aroundId string) ([]*discordgo.Message, error) {
+	messages, err := b.session.ChannelMessages(b.channelId, limit, beforeId, afterId, aroundId)
+
 	if err != nil {
 		fmt.Println(err)
 	}
-	for _, message := range messages {
-		fmt.Printf("Message: %s \n", message.Content)
-	}
+
+	return messages, err
 }
 
-func (b DiscordBot) ReadMessageInLastWeek(session *discordgo.Session, channelId string) {
+func (b *DiscordBot) readMessagesInLastWeek() ([]*discordgo.Message, error) {
 	now := time.Now()
 	oneWeekAgo := now.AddDate(0, 0, -7)
 
-	messages, err := session.ChannelMessages(channelId, 100, "", "", "")
-	if err != nil {
-		fmt.Println(err)
-	}
+	messages, err := b.GetAllMessagesFromChannel(100, "", "", "")
 
 	var filteredMessages []*discordgo.Message
 	for _, message := range messages {
@@ -85,9 +93,28 @@ func (b DiscordBot) ReadMessageInLastWeek(session *discordgo.Session, channelId 
 			filteredMessages = append(filteredMessages, message)
 		}
 	}
+
+	return filteredMessages, err
 }
 
-func (b DiscordBot) Close() {
-	b.currentSession.Close()
-	fmt.Println("Bot session closed")
+func (b *DiscordBot) CalculateScoreboard() {
+	messages, err := b.readMessagesInLastWeek()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, message := range messages {
+		fmt.Println(message.Content)
+	}
+}
+
+func (b *DiscordBot) getTimestampFromSnowFlake(snowflake string) (time.Time, error) {
+	snowflakeAsInt, err := strconv.ParseInt(snowflake, 10, 64)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	return time.Unix(((snowflakeAsInt>>22)+1288834974657)/1000, 0).UTC(), err
 }
