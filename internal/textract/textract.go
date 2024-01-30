dba1ae0eb619ebbe4c5fae70fd01c070d5ef73ad
Collin Brandt <47092431+cbrandt6@users.noreply.github.com>
2024-01-29 20:57:26 -0500

    Image proc (#2)
    * Parses lines from boostcamp image
    
    * Ignores image dir
    
    * Uses image processing to calculate scoreboard
    
    * Parses lines from Boostcamp image

--- internal/textract/textract.go
+++ internal/textract/textract.go
@@ -3,11 +3,14 @@ package textract
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/textract"
 )
+
+const ImageDir = "./internal/textract/images"
 
 type TextractWrapper struct {
 	session *textract.Textract
@@ -18,10 +21,14 @@ func NewTextractWrapper() *TextractWrapper {
 	wrapper.session = textract.New(session.Must(session.NewSession(&aws.Config{
 		Region: aws.String("us-east-1"),
 	})))
+
+	clearImageDir()
+	return wrapper
 }
 
-func (t TextractWrapper) ParseTextFromImage() {
-	file, err := os.ReadFile("sample.png")
+func (t TextractWrapper) ParseTextLinesFromImage(filePath string) ([]string, error) {
+	file, err := os.ReadFile(filePath)
+
 	if err != nil {
 		panic(err)
 	}
@@ -31,15 +38,50 @@ func (t TextractWrapper) ParseTextFromImage() {
 			Bytes: file,
 		},
 	})
+
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(resp)
+	lines := make([]string, 0, len(resp.Blocks))
 
-	for i := 1; i < len(resp.Blocks); i++ {
-		if *resp.Blocks[i].BlockType == "WORD" {
-			fmt.Println(*resp.Blocks[i].Text)
+	for _, block := range resp.Blocks {
+		if *block.BlockType == "LINE" {
+			lines = append(lines, *block.Text)
+		}
+	}
+
+	return lines, err
+}
+
+func clearImageDir() {
+	dir, err := os.Open(ImageDir)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer dir.Close()
+
+	fileInfos, err := dir.Readdir(-1)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Iterate over each file and delete it
+	for _, fileInfo := range fileInfos {
+		filePath := filepath.Join(ImageDir, fileInfo.Name())
+
+		// Check if it's a regular file (not a directory)
+		if fileInfo.Mode().IsRegular() {
+			// Delete the file
+			err := os.Remove(filePath)
+			if err != nil {
+				fmt.Printf("Error deleting file %s: %s\n", filePath, err)
+			} else {
+				fmt.Printf("Deleted file: %s\n", filePath)
+			}
 		}
 	}
 }
