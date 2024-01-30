827168b108bccd60de003992cd4d41fc4a819009
Collin <cbrandt6@ycp.edu>
2024-01-29 20:55:40 -0500

    Parses lines from Boostcamp image

--- internal/textract/textract.go
+++ internal/textract/textract.go
@@ -1,12 +1,16 @@
 package textract
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/textract"
 )
+
+const ImageDir = "./internal/textract/images"
 
 type TextractWrapper struct {
 	session *textract.Textract
@@ -17,11 +21,13 @@ func NewTextractWrapper() *TextractWrapper {
 	wrapper.session = textract.New(session.Must(session.NewSession(&aws.Config{
 		Region: aws.String("us-east-1"),
 	})))
+
+	clearImageDir()
 	return wrapper
 }
 
-func (t TextractWrapper) ParseTextLinesFromImage() ([]string, error) {
-	file, err := os.ReadFile("./test/IMG_1949.png")
+func (t TextractWrapper) ParseTextLinesFromImage(filePath string) ([]string, error) {
+	file, err := os.ReadFile(filePath)
 
 	if err != nil {
 		panic(err)
@@ -46,4 +52,36 @@ func (t TextractWrapper) ParseTextLinesFromImage() ([]string, error) {
 	}
 
 	return lines, err
+}
+
+func clearImageDir() {
+	dir, err := os.Open(ImageDir)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer dir.Close()
+
+	fileInfos, err := dir.Readdir(-1)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Iterate over each file and delete it
+	for _, fileInfo := range fileInfos {
+		filePath := filepath.Join(ImageDir, fileInfo.Name())
+
+		// Check if it's a regular file (not a directory)
+		if fileInfo.Mode().IsRegular() {
+			// Delete the file
+			err := os.Remove(filePath)
+			if err != nil {
+				fmt.Printf("Error deleting file %s: %s\n", filePath, err)
+			} else {
+				fmt.Printf("Deleted file: %s\n", filePath)
+			}
+		}
+	}
 }
